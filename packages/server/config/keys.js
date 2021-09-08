5fce1232c5bd77c470365e4ee31a3e6e0d39d11a
Mia Peace <miapeace@Kenzies-MacBook-Pro.local>
2021-09-08 08:24:14 -0800

    MongoDB Atlas hooked up, starting debug process

---
+++ packages/server/config/keys.js
@@ -0,0 +1,15 @@
+let gitRepoName = require('git-repo-name');
+let repoName = gitRepoName.sync("../../");
+module.exports = {
+  app: {
+    name: repoName,
+    apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
+  },
+  database: {
+    url: `mongodb+srv://mxsilentwar:103117Dp!@teaching.sll1f.mongodb.net/testerson?retryWrites=true&w=majority`,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET || 'jwt-secret',
+    tokenLife: '7d',
+  },
+}
